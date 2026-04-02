1e8248737eddb9e61d2be4496ba79fa54dcd6246
wuyuphoebe <wuyuphoebe@gmail.com>
2026-04-03 02:16:08 +0800

    UUU2

--- data.js
+++ data.js
@@ -68,14 +68,120 @@ const FB_DATA = [
         lat: 13.9642237,
         lng: 100.5893724,
         images: [
-            { url: "images/COK Cream of The Koff/1.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
-            { url: "images/COK Cream of The Koff/2.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
-            { url: "images/COK Cream of The Koff/3.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
-            { url: "images/COK Cream of The Koff/4.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
-            { url: "images/COK Cream of The Koff/5.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
-            { url: "images/COK Cream of The Koff/6.mp4", platform: "Instagram", category: "life", author: "Freen & Becky" }
+            { url: "images/COK Cream of The Koff/FreenBecky_20210911-1.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/FreenBecky_20210911-2.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/FreenBecky_20210911-3.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/FreenBecky_20210911-4.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/FreenBecky_20210911-5.PNG", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/FreenBecky_20210911-6.mp4", platform: "Instagram", category: "life", author: "Freen & Becky" }
         ],
         mapUrl: "https://maps.app.goo.gl/oH7iLpdCqxeg8GKw6",
         desc: "FB約會的咖啡廳"
+    },
+    {
+        id: 5,
+        name: "FigureGround Cafe",
+        celebrity: "Freen",
+        date: "2022/9/21",
+        lat: 13.7886601,
+        lng: 100.5606651,
+        images: [
+            { url: "images/FigureGround Cafe/Freen_20220921-1.PNG", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/FigureGround Cafe/Freen_20220921-2.PNG", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/FigureGround Cafe/Freen_20220921-3.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/FigureGround Cafe/Freen_20220921-4.PNG", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/FigureGround Cafe/Freen_20220921-5.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/FigureGround Cafe/Freen_20220921-6.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/FigureGround Cafe/Freen_20220921-7.jpg", platform: "Instagram", category: "life", author: "Freen" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/oH7iLpdCqxeg8GKw6",
+        desc: "生活照"
+    },
+    {
+        id: 6,
+        name: "99 Home Studio 117",
+        celebrity: "Freen & Becky",
+        date: "2022",
+        lat: 13.821791,
+        lng: 100.7092814,
+        images: [
+            { url: "images/GAP/FreenBecky_2022-1.jpg", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-2.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-3.jpg", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-4.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-5.jpg", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-6.jpg", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-7.jpg", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/GAP/FreenBecky_2022-8.jpg", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/jGy1oXhJkfrVWw9U8",
+        desc: "GAP The Series 拍攝地/Sam的家"
+    },
+    {
+        id: 7,
+        name: "Kaembum Fresh Coffee Branch 2 By Nun",
+        celebrity: "Freen",
+        date: "2021",
+        lat: 13.383783,
+        lng: 101.6445084,
+        images: [
+            { url: "images/Kaembum Fresh Coffee/Freen_2021-1.jpg", platform: "Facebook", category: "coffee shop", author: "Freen" },
+            { url: "images/Kaembum Fresh Coffee/Freen_2021-2.jpg", platform: "Facebook", category: "coffee shop", author: "Freen" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/L7eKoJMjEMAB9nCZ7",
+        desc: "Freen阿姨的咖啡廳"
+    },
+    {
+        id: 8,
+        name: "蘭實大學 Rangsit University",
+        celebrity: "Freen",
+        date: "2022",
+        lat: 13.9653262,
+        lng: 100.568843,
+        images: [
+            { url: "images/Rangsit/Freen_2022-1.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-2.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-3.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-4.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-5.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-6.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-7.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-8.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" },
+            { url: "images/Rangsit/Freen_2022-9.jpg", platform: "Instagram,Twitter", category: "university", author: "Freen" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/aMvKFY2JR4MdJ7YV8",
+        desc: "Freen的大學"
+    },
+    {
+        id: 9,
+        name: "RSU Music Academy Conservatory of Music Rangsit University",
+        celebrity: "Freen & Becky",
+        date: "2022",
+        lat: 13.9662864,
+        lng: 100.5811218,
+        images: [
+            { url: "images/RSU Music Academy Conservatory of Music Rangsit University/Freen_2022-1-1.jpg", platform: "Twitter", category: "university", author: "Freen" },
+            { url: "images/RSU Music Academy Conservatory of Music Rangsit University/Freen_2022-1-2.jpg", platform: "Twitter", category: "university", author: "Freen" },
+            { url: "images/RSU Music Academy Conservatory of Music Rangsit University/Freen_2022-1-3.jpg", platform: "Twitter", category: "university", author: "Freen" },
+            { url: "images/RSU Music Academy Conservatory of Music Rangsit University/Freen_2022-1-4.jpg", platform: "Twitter", category: "university", author: "Freen" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/aQ1BzCbCHWJjE65z7",
+        desc: "Freen畢業當天的直播現場"
+    },
+    {
+        id: 10,
+        name: "Woolloomooloo Bakery",
+        celebrity: "Freen",
+        date: "2022/07/01",
+        lat: 13.7966324,
+        lng: 100.5655901,
+        images: [
+            { url: "images/Woolloomooloo Bakery Ratchada/Freen_20220701-1.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Woolloomooloo Bakery Ratchada/Freen_20220701-2.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Woolloomooloo Bakery Ratchada/Freen_20220701-3.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Woolloomooloo Bakery Ratchada/Freen_20220701-4.jpg", platform: "Instagram", category: "life", author: "Freen" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/eQGRx4dfcuh6wMFZA",
+        desc: "生活照"
     }
 ];
