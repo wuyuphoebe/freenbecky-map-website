839c793c46452324ccdd47ef44e81ebf0bde5dbb
wuyuphoebe <wuyuphoebe@gmail.com>
2026-04-03 00:09:24 +0800

    試一些地點上去

---
+++ data.js
@@ -0,0 +1,82 @@
+const FB_DATA = [
+    {
+        id: 1,
+        name: "Mother Roaster Talat Noi",
+        celebrity: "Freen",
+        date: "2020/09/27",
+        lat: 13.7329961,
+        lng: 100.5097469,
+        images: [
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-1.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-2.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-3.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-4.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-5.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-6.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-7.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-8.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-9.jpg", platform: "Instagram", category: "life", author: "Freen" },
+            { url: "images/Mother Roaster Talat Noi/Freen_20200927-10.jpg", platform: "Instagram", category: "life", author: "Freen" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/DcC7Qo9tRKK1Uee9A",
+        desc: "生活照"
+    },
+    {
+        id: 2,
+        name: "WAWA House",
+        celebrity: "Freen & Becky",
+        date: "2024",
+        lat: 13.8235644,
+        lng: 100.3663811,
+        images: [
+            { url: "images/Wa Wa House/1.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/2.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/3.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/4.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/5.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/6.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/7.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/8.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/9.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/10.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" },
+            { url: "images/Wa Wa House/11.png", platform: "Instagram,Twitter,Youtube", category: "act", author: "idolfactory" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/9o7FQBSgev1kuDXE9",
+        desc: "The Loyal Pin 拍攝地"
+    },
+    {
+        id: 3,
+        name: "BLOCK I",
+        celebrity: "Becky",
+        date: "2024/05/26",
+        lat: 13.7444971,
+        lng: 100.5296505,
+        images: [
+            { url: "images/BLOCK I/1.jpg", platform: "Instagram", category: "life", author: "Becky" },
+            { url: "images/BLOCK I/2.jpg", platform: "Instagram", category: "life", author: "Becky" },
+            { url: "images/BLOCK I/3.jpg", platform: "Instagram", category: "life", author: "Becky" },
+            { url: "images/BLOCK I/4.jpg", platform: "Instagram", category: "life", author: "Becky" },
+            { url: "images/BLOCK I/5.jpg", platform: "Instagram", category: "life", author: "Becky" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/agUqyhnLf7TtQGfM8",
+        desc: "生活照"
+    },
+    {
+        id: 4,
+        name: "COK Cream of The Koff",
+        celebrity: "Freen & Becky",
+        date: "2021/9/11",
+        lat: 13.9642237,
+        lng: 100.5893724,
+        images: [
+            { url: "images/COK Cream of The Koff/1.png", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/2.png", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/3.png", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/4.png", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/5.png", platform: "Instagram", category: "life", author: "Freen & Becky" },
+            { url: "images/COK Cream of The Koff/6.mp4", platform: "Instagram", category: "life", author: "Freen & Becky" }
+        ],
+        mapUrl: "https://maps.app.goo.gl/oH7iLpdCqxeg8GKw6",
+        desc: "FB約會的咖啡廳"
+    }
+];
